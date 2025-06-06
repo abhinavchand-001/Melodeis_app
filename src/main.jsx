a7d243daa1177129a85670a4388074473c302206
abhinavchand-001 <abhiforrandom50@gmail.com>
2025-06-06 23:16:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import App from "./App.jsx";
 import ReactDom from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
-// import { Provider } from "react-redux";
-// import { store } from "./redux/store";
+
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  
     <BrowserRouter>
       <Auth0Provider 
       domain="dev-2qtcevcr8mdl1pj7.us.auth0.com"
@@ -20,5 +19,5 @@ createRoot(document.getElementById("root")).render(
         <App />
       </Auth0Provider>
     </BrowserRouter>
-  </Provider>
+  
 );
